c6d1d68853314364194e1458258e589c8c21eaa2
Ivan <ivansamwabina@gmail.com>
2023-07-17 20:33:09 +0800

    Added Category and SubCategory Setup

--- resources/views/backend/category/category_all.blade.php
+++ resources/views/backend/category/category_all.blade.php
@@ -6,19 +6,19 @@
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-        <div class="breadcrumb-title pe-3">All Brand</div>
+        <div class="breadcrumb-title pe-3">All Category</div>
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">All Brand</li>
+                    <li class="breadcrumb-item active" aria-current="page">All Category</li>
                 </ol>
             </nav>
         </div>
         <div class="ms-auto">
             <div class="btn-group">
-                <a class="btn btn-primary" href="{{route('add.brand')}}">Add Brand</a>
+                <a class="btn btn-primary" href="{{route('add.category')}}">Add Category</a>
 
             </div>
         </div>
@@ -32,20 +32,20 @@
                     <thead>
                         <tr>
                             <th>SL</th>
-                            <th>Brand Name</th>
-                            <th>Brand Image</th>
+                            <th>Category Name</th>
+                            <th>Category Image</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($brands as $key => $item)
+                        @foreach ($categories as $key => $item)
                         <tr>
                             <td>{{$key+1}}</td>
-                            <td>{{$item->brand_name}}</td>
-                            <td><img src="{{asset($item->brand_image)}}" style="width:70px; height:40px;" alt=""></td>
+                            <td>{{$item->category_name}}</td>
+                            <td><img src="{{asset($item->category_image)}}" style="width:70px; height:40px;" alt=""></td>
                             <td>
-                                <a href="{{route('edit.brand', $item->id)}}" class="btn btn-info">Edit</a>
-                                <a href="{{route('delete.brand', $item->id)}}" class="btn btn-danger" id="delete">Delete</a>
+                                <a href="{{route('edit.category', $item->id)}}" class="btn btn-info">Edit</a>
+                                <a href="{{route('delete.category', $item->id)}}" class="btn btn-danger" id="delete">Delete</a>
                             </td>
                         </tr>
 
@@ -54,8 +54,8 @@
                     <tfoot>
                         <tr>
                             <th>SL</th>
-                            <th>Brand Name</th>
-                            <th>Brand Image</th>
+                            <th>Category Name</th>
+                            <th>Category Image</th>
                             <th>Action</th>
                         </tr>
                     </tfoot>
